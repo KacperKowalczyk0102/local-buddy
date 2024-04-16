e84d784f2d4630ad09bafc34d38f40fb5a9c2139
KucielKrzysztof <s223871@student.uek.krakow.pl>
2024-04-17 00:19:10 +0200

    adds bootstrap and sass to project

--- local-buddy/src/main.js
+++ local-buddy/src/main.js
@@ -1,18 +1,19 @@
-import { createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
-import { firebaseConfig } from '../public/firebaseConfig';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { firebaseConfig } from "../public/firebaseConfig";
 import { initializeApp } from "firebase/app";
-
-
+import Vue from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "./assets/css/main.scss";
+//use bootstrap
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
-
 
 const app = createApp(App);
 
 app.use(router);
 
-app.mount('#app');
-
+app.mount("#app");
--- local-buddy/src/views/Home.vue
+++ local-buddy/src/views/Home.vue
@@ -1,14 +1,50 @@
 <template>
-    Home
-  </template>
-  
-  <script>
-  export default {
-    // Twój kod JavaScript
-  }
-  </script>
-  
-  <style>
-  /* Opcjonalne style CSS */
-  </style>
-  
+	Home
+	<button v-if="isUserLoggedIn" @click="logout" class="btn btn-primary">Wyloguj</button>
+	<div class="container">aaa</div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const auth = getAuth();
+
+export default {
+	name: "Home",
+	setup() {
+		const router = useRouter();
+		const isUserLoggedIn = ref(false);
+
+		const logout = async () => {
+			try {
+				await signOut(auth);
+				console.log("Wylogowano pomyślnie");
+			} catch (error) {
+				console.error("Błąd podczas wylogowywania", error);
+			}
+		};
+
+		onMounted(() => {
+			console.log("Home mounted");
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					// Użytkownik jest zalogowany.
+					console.log("Użytkownik jest zalogowany");
+					isUserLoggedIn.value = true;
+					/* router.push("/feed"); */
+				} else {
+					// Użytkownik jest wylogowany.
+					console.log("Użytkownik jest wylogowany");
+					isUserLoggedIn.value = false;
+				}
+			});
+		});
+
+		return { logout, isUserLoggedIn };
+	},
+};
+</script>
+
+<style></style>
