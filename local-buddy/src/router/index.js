fd6fc9bbef786fca6ddde10780de8930ad488676
Kacper <s223667@student.uek.krakow.pl>
2024-04-16 19:59:34 +0200

    adding user authorization

--- local-buddy/src/router/index.js
+++ local-buddy/src/router/index.js
@@ -1,4 +1,6 @@
+
 import { createRouter, createWebHistory } from 'vue-router';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -6,8 +8,40 @@ const router = createRouter({
         { path: '/', name: 'home', component: () => import('../views/Home.vue') },
         { path: '/register', name: 'register', component: () => import('../views/Register.vue') },
         { path: '/signin', name: 'signin', component: () => import('../views/SignIn.vue') },
-        { path: '/feed', name: 'feed', component: () => import('../views/Feed.vue') },
+        { path: '/feed', name: 'feed', component: () => import('../views/Feed.vue'),
+            meta: {
+                requiresAuth: true
+        } },
+
     ]
+});
+
+const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+       const removeListener = onAuthStateChanged(
+        getAuth(),
+        (user) => {
+           removeListener();
+              resolve(user);
+        },
+        reject
+         );
+    }
+    );
+}
+
+
+router.beforeEach(async(to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresAuth)) {
+      if(await getCurrentUser()) {
+        next();
+      }else {
+        alert("Dostęp tylko dla zalogowanych użytkowników");
+        next("/");
+      }     
+    }else {
+      next();
+    }
 });
 
 export default router;
--- local-buddy/src/views/SignIn.vue
+++ local-buddy/src/views/SignIn.vue
@@ -1,14 +1,60 @@
 <template>
-Sign in
-  </template>
-  
-  <script>
-  export default {
-    // Twój kod JavaScript
-  }
-  </script>
-  
-  <style>
-  /* Opcjonalne style CSS */
-  </style>
-  
+  <h1>Zaloguj się</h1>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Hasło" v-model="password" /></p>
+  <p v-if="errMsg">{{ errMsg }}</p>
+  <p><button @click="logIn">Zaloguj się</button></p>
+  <p><button @click="logInWithGoogle">Zaloguj się z google</button></p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider,signInWithPopup } from 'firebase/auth';
+import { useRouter } from 'vue-router';
+
+const email = ref('');
+const password = ref('');
+const errMsg = ref('');// ERROR MESSAGE
+const router = useRouter();
+
+const logIn = () => {
+  signInWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      console.log("Zarejestrowano pomyślnie")
+      console.log(auth.currentUser)
+      router.push('/feed');
+    })
+    .catch((error) => {
+      console.log(error.code);
+      switch(error.code){
+        case 'auth/invalid-email':
+          errMsg.value = 'Niepoprawny email';
+          break;
+        case 'auth/user-not-found':
+          errMsg.value = 'Użytkownik nie istnieje';
+          break;
+        case 'auth/wrong-password':
+          errMsg.value = 'Niepoprawne hasło';
+          break;
+        default:
+          errMsg.value = 'Email bądź hasło są niepoprawne';
+          break;
+      }
+    });
+};
+
+const logInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      console.log(result.user);
+      router.push('/feed');
+    }).catch((error) => {
+
+    });
+};
+</script>
+
+<style>
+/* Opcjonalne style CSS */
+</style>
