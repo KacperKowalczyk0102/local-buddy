ece8018acc6d13d01ec33ed4c4bbe1122e769bd5
Mateusz Kozieł <s223577@student.uek.krakow.pl>
2024-04-23 22:45:06 +0200

    dodano przycisk lokalizacji

--- local-buddy/src/views/Post.vue
+++ local-buddy/src/views/Post.vue
@@ -22,12 +22,24 @@
       </div>
       <div class="form-group">
         <label for="location">Lokalizacja:</label>
-        <input type="text" id="location" v-model="location" @input="searchSuggestions" />
+        <div class="input-group custom-input-group">
+          <input type="text" id="location" v-model="location" @input="searchSuggestions" class="form-control" />
+          <div class="input-group-append">
+            <button class="btn btn-custom" type="button" @click="getCurrentLocation">
+              <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
       <ul class="autosuggest-list" v-if="suggestions.length > 0">
         <li v-for="suggest in suggestions" :key="suggest.place_id" @click="selectSuggestion(suggest)">{{ suggest.description }}</li>
       </ul>
-      <button type="submit">Dodaj post</button>
+
+
+      <button @Click="vibrate()" type="submit">Dodaj post</button>
     </form>
   </div>
 </template>
@@ -58,6 +70,41 @@ export default {
   };
   },
   methods: {
+    getCurrentLocation() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const latLng = new google.maps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          const geocoder = new google.maps.Geocoder();
+          geocoder.geocode({ location: latLng }, (results, status) => {
+            if (status === google.maps.GeocoderStatus.OK && results[0]) {
+              this.location = results[0].formatted_address;
+              // Aktualizuj sugestie, jeśli potrzeba
+              this.searchSuggestions();
+            } else {
+              console.error("Nie udało się znaleźć lokalizacji: ", status);
+            }
+          });
+        },
+        error => {
+          console.error("Błąd podczas uzyskiwania lokalizacji: ", error);
+        }
+      );
+    } else {
+      console.error("Twoja przeglądarka nie obsługuje Geolocation API");
+    }
+  },
+    vibrate() {
+      if ("vibrate" in navigator) {
+        navigator.vibrate([100, 100, 100, 100, 100, 100, 500, 100, 500, 100, 500, 100, 100, 100, 100, 100, 100]);
+        console.log('poszło');
+      } else {
+        alert("Vibration not supported");
+      }
+    },
     searchSuggestions() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
@@ -131,6 +178,28 @@ export default {
 
 .form-group {
   margin-bottom: 20px;
+}
+
+.btn-custom {
+  background-color: white;
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 24px; /* Zwiększamy padding dla większego przycisku */
+  font-size: 18px; /* Zwiększamy rozmiar tekstu */
+  width: auto; 
+  height: auto;
+}
+  
+.btn-custom svg {
+  width: 1.2em; /* Zwiększamy rozmiar ikony */
+  height: 1.2em; /* Zwiększamy rozmiar ikony */
+  margin-right: 5px;
+  fill: black;
+  vertical-align: middle;
 }
 
 label {
