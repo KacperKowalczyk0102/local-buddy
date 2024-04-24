1a82357c16ccaf878f4aa94f8da4d0ed029b6163
KucielKrzysztof <s223871@student.uek.krakow.pl>
2024-04-25 00:10:49 +0200

    Stopped tracking src/firebaseConfig.js

--- local-buddy/src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-	apiKey: "AIzaSyDJeUcWw1aB7QE80CpE56lsFJAA6War48w",
-	authDomain: "localbuddy-6a06e.firebaseapp.com",
-	projectId: "localbuddy-6a06e",
-	storageBucket: "localbuddy-6a06e.appspot.com",
-	messagingSenderId: "965692683175",
-	appId: "1:965692683175:web:5a65a4bc92849c0cdefc82",
-	measurementId: "G-XF8Z2TQKYG",
-};
